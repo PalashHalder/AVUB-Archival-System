edc7e63d05857f6b191296cf51e4eff0b36e06f6
PalashHalder <halder.palash08@gmail.com>
2023-07-20 15:29:27 +0600

    web view complete

--- app/src/main/java/com/example/avubarchivalsystem/MainActivity.java
+++ app/src/main/java/com/example/avubarchivalsystem/MainActivity.java
@@ -1,14 +1,68 @@
 package com.example.avubarchivalsystem;
 
+import static com.example.avubarchivalsystem.R.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    WebView webView;
+    ProgressBar progressBar;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
+
+        progressBar = findViewById(id.progessBar);
+
+        webView = findViewById(id.webView);
+
+        webView.loadUrl("https://javascript.info/");
+
+        webView.setWebViewClient( new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                progressBar.setVisibility(View.VISIBLE);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+                super.onPageFinished(view, url);
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+
+                super.onProgressChanged(view, newProgress);
+
+                progressBar.setProgress(newProgress);
+            }
+        });
+
+    }
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
+
+
+
---
+++ app/src/main/java/com/example/avubarchivalsystem/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.avubarchivalsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+public class SplashActivity extends AppCompatActivity {
+
+    CountDownTimer countDownTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        countDownTimer = new CountDownTimer(3000, 100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+                startActivity(intent);
+
+            }
+        }.start();
+    }
+}
